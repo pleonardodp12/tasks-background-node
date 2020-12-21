06038e0cc51990e2fc082f0757519f13de9e2181
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-12-21 10:23:14 -0300

    feat: configured queue

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,4 @@
 import passwordGenerator from 'password-generator';
-import Mail from '../lib/Mail';
 
 export default {
     async store(req, res) {
@@ -10,13 +9,6 @@ export default {
             email,
             password: passwordGenerator(15, false)
         };
-
-        Mail.sendMail({
-            from: 'Leo <pauloleonardodp@gmail.com',
-            to: `${name} <${email}`,
-            subject: 'Cadastro de usuário',
-            html: `Olá, ${name}, bem vindo ao gerenciador de tasks de email.`
-        })
 
         return res.json(user)
     }
